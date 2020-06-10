d91db12251c9aec105084d6604dbb0698fc3440f
arief-source <66731570+arief-source@users.noreply.github.com>
2020-06-10 19:28:02 +0700

    Add files via upload

---
+++ angka.py
@@ -0,0 +1,28 @@
+#Data Type
+
+print ('halodunia')
+usia = 11
+x = ('jawaban1')
+print (x+' '+str(usia + 11))
+#USIA
+
+usiaAndi = 40
+usiaBudi = 20
+
+usiaAndi += 3
+usiaBudi %= 3
+
+print (usiaAndi)
+print (usiaBudi)
+
+# Arimatika
+import math
+
+usiaandi = 26
+usiabudi = 20
+
+x = ('jawaban')
+y = usiaandi*usiabudi
+
+print (x+str(y))
+print (x+str(math.ceil(y)))
